1f8be6d9718b89249e7ecdf3451ac4ffc328a55f
Manuel1611 <garciarquema@gmail.com>
2021-01-11 10:25:42 +0100

    Update MainActivity.java Manu

--- app/src/main/java/teambiscochito/toptrumpsgame/MainActivity.java
+++ app/src/main/java/teambiscochito/toptrumpsgame/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.d("Hola", "Soy Oleg");
+        Log.d("Hola", "Soy Manu");
     }
 }
