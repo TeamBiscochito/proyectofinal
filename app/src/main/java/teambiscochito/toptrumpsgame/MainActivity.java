9fdbd95a28189f5738e7bc2d7271ac672739f280
Oleg37 <iofernandezllebrez134@ieszaidinvergeles.org>
2021-01-11 10:23:29 +0100

    update MainActivity.java

--- app/src/main/java/teambiscochito/toptrumpsgame/MainActivity.java
+++ app/src/main/java/teambiscochito/toptrumpsgame/MainActivity.java
@@ -3,6 +3,7 @@ package teambiscochito.toptrumpsgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("Hola", "Soy Oleg");
     }
 }
