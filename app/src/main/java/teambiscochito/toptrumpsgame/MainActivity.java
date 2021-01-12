2fa076f30d3226fa5c515515ac0c3ae0df072172
Gabriguexp <73891498+gabriguexp@users.noreply.github.com>
2021-01-12 11:12:41 +0100

    Añadido Receiver de bateria.

--- app/src/main/java/teambiscochito/toptrumpsgame/MainActivity.java
+++ app/src/main/java/teambiscochito/toptrumpsgame/MainActivity.java
@@ -2,18 +2,35 @@ package teambiscochito.toptrumpsgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    MyReceiver receiver;
+    IntentFilter intentFilter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("Hola", "Soy Oleg");
-        Log.d("Hola", "Soy Manu");
-        Log.d("Tag", "Buenas soy Gabri");
+        MyReceiver receiver;
+        IntentFilter intentFilter;
+        receiver = new MyReceiver();
+        intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_BATTERY_LOW);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(receiver,intentFilter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(receiver);
     }
 }
