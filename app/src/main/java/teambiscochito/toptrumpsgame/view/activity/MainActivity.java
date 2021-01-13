b59e58b59c201b4b9247485ad3170d535d92962e
Oleg37 <iofernandezllebrez134@ieszaidinvergeles.org>
2021-01-13 14:28:45 +0100

    add navigation drawer

--- app/src/main/java/teambiscochito/toptrumpsgame/view/activity/MainActivity.java
+++ app/src/main/java/teambiscochito/toptrumpsgame/view/activity/MainActivity.java
@@ -1,12 +1,19 @@
 package teambiscochito.toptrumpsgame.view.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.WindowManager;
+
+import com.google.android.material.navigation.NavigationView;
 
 import teambiscochito.toptrumpsgame.MyReceiver;
 import teambiscochito.toptrumpsgame.R;
@@ -38,5 +45,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         unregisterReceiver(receiver);
+    }
+
+    public void inicioDrawer() {
+        // TODO: Drawer en caso de que lo queramos
+//        DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+//        toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        ctlLayout = findViewById(R.id.ctlLayout);
+//        appBarLayout = findViewById(R.id.appBarLayout);
+//
+//        NavigationView navigationView = findViewById(R.id.navigationViewMain);
+//
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+//
+//        AppBarConfiguration appBarConfiguration =
+//                new AppBarConfiguration.Builder(navController.getGraph()).setOpenableLayout(drawerLayout).build();
+//
+//        NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration);
+//
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        NavigationUI.setupWithNavController(navigationView, navController);
+//
+//        toolbarCollab(navController);
     }
 }
