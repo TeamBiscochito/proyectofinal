b160848828940b27622302e627534c7383ea802b
Manuel1611 <garciarquema@gmail.com>
2021-01-13 10:48:25 +0100

    Update choose user 2

--- app/src/main/java/teambiscochito/toptrumpsgame/view/fragment/ChooseUserFragment.java
+++ app/src/main/java/teambiscochito/toptrumpsgame/view/fragment/ChooseUserFragment.java
@@ -2,11 +2,14 @@ package teambiscochito.toptrumpsgame.view.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import teambiscochito.toptrumpsgame.R;
 
@@ -24,5 +27,10 @@ public class ChooseUserFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_choose_user, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
