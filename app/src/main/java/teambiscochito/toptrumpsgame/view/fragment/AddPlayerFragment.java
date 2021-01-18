0f36b207a9e4e3503933e9af8e8c9234e3ec7487
Manuel1611 <garciarquema@gmail.com>
2021-01-18 10:04:52 +0100

    Admin screen y mejoras, interrumpido

---
+++ app/src/main/java/teambiscochito/toptrumpsgame/view/fragment/AddPlayerFragment.java
@@ -0,0 +1,40 @@
+package teambiscochito.toptrumpsgame.view.fragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import teambiscochito.toptrumpsgame.R;
+
+public class AddPlayerFragment extends Fragment {
+
+    ViewPager2 vp_avatar;
+
+    public AddPlayerFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_add_player, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        vp_avatar = view.findViewById(R.id.vp_avatar);
+    }
+}
