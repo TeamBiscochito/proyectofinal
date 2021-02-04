725f9fb4aa21265171e1e91d77cc1baea35e605c
Oleg37 <iofernandezllebrez134@ieszaidinvergeles.org>
2021-02-04 05:33:07 +0100

    update Copyright

--- app/src/main/java/teambiscochito/toptrumpsgame/model/laravel/CardClient.java
+++ app/src/main/java/teambiscochito/toptrumpsgame/model/laravel/CardClient.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright (c) 2021. Team Biscochito.
+ *
+ * Licensed under the GNU General Public License v3.0
+ *
+ * https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * Permissions of this strong copyleft license are conditioned on making available complete
+ * source code of licensed works and modifications, which include larger works using a licensed
+ * work, under the same license. Copyright and license notices must be preserved. Contributors
+ * provide an express grant of patent rights.
+ */
+
 package teambiscochito.toptrumpsgame.model.laravel;
 
 import java.util.ArrayList;
